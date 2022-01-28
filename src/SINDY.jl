4c342b4c137e3b8a04e227b82b0dfd9ed7f00aad
Rahul Manavalan <rahul.manavalan@tum.de>
2022-01-28 16:24:35 +0100

    Included files highlighting the basic structure of the lib

--- src/SINDY.jl
+++ src/SINDY.jl
@@ -1,5 +1,7 @@
 module SINDY
-
-greet() = print("Bear with us while we install SINDY.jl - a library for sparse identification of Dynamical Systems.")
-
-end # module
+    greet() = print("Bear with us while we install SINDY.jl - a library for sparse identification of Dynamical Systems.")
+    include("NumDiff.jl")
+    include("Library.jl")
+    include("Optimizers.jl")
+    include("Parsers.jl")
+end
