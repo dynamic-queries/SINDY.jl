906cbb2888bc0cdb33657f0f07d84c78018174ad
Rahul Manavalan <rahul.manavalan@tum.de>
2022-02-10 05:16:28 +0100

    Added pretty print, Automating remake is still frustrating

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,4 +1,6 @@
 include("../src/Library.jl")
+using DataFrames
+using CSV
 
 function denoise!(sol)
 
@@ -14,12 +16,36 @@ function munge(A::Vector{Vector{Float64}})
     M
 end
 
+function pprint(x::Matrix,btype::AbstractBasis)
+    if size(x,2) == 3 && typeof(btype) == PolyTrigBasis
+        labels = ["1","x","y","z","x²","xy","xz","y²","yz","z²",
+                 "x³","x²y","x²z","xy²","y³","y²z","xz²","yz²","z³","sin(x)","sin(y)","sin(z)"
+                 ,"cos(x)","cos(y)","cos(z)","tan(x)","tan(y)","tan(z)"]
+    elseif size(x,2) == 3 && typeof(btype) == PolynomialBasis
+        labels = ["1","x","y","z","x²","xy","xz","y²","yz","z²",
+                 "x³","x²y","x²z","xy²","y³","y²z","xz²","yz²","z³"]
+
+    elseif size(x,2) == 2 && typeof(btype) == PolyTrigBasis
+        labels = ["1","x","y","x²","xy","y²",
+                 "x³","x²y","xy²","y³","sin(x)","sin(y)"
+                 ,"cos(x)","cos(y)","tan(x)","tan(y)"]
+    elseif size(x,2) == 2 && typeof(btype) == PolynomialBasis
+        labels = ["1","x","y","x²","xy","y²",
+                 "x³","x²y","xy²","y³"]
+    end
+    df = DataFrame(basis=labels)
+    df2 = DataFrame(x,:auto)
+    print(hcat(df,df2))
+end
+
 struct LocalBasis <: AbstractBasis
     polyorder::Int
+    type::AbstractBasis
 
 end
 
-
 function _remake(ξ,type::AbstractBasis)
+    function f(du,u,p,t)
 
+    end
 end
--- test/SINDY.jl
+++ test/SINDY.jl
@@ -12,15 +12,31 @@ lib1 = PolyTrigBasis()
 # Integration tests - lorenz attractor
 
 data = datagen(LorenzSystem())
-savefig("../figures/Traj_Lorenz.svg")
+savefig("./figures/Traj_Lorenz.svg")
 v = differentiate(data,TotalVariationalDerivativative())
-savefig("../figures/Vel_Lorenz.svg")
+savefig("./figures/Vel_Lorenz.svg")
 b_type = PolyTrigBasis()
 θ = basis(munge(data.u),b_type)
 LinearAlgebra.cond(θ)
 ξ = _optimize(θ,v,STLSQ(0.1))
+pprint(ξ,b_type)
+
 _remake(ξ,b_type)
 savefig("../figures/Remade_Lorenz.svg")
 
 #TODO: The basis function is the only manual part of the implementation. This has to change.
 #TODO: Include the forcing function expressions in the defn of the Dynamical System.
+
+# Integration tests - Lotka Volterra
+data = datagen(LotkaVolterra())
+savefig("./figures/Traj_Lotka.svg")
+v = differentiate(data,TotalVariationalDerivativative())
+savefig("./figures/Vel_Lotka.svg")
+b_type = PolynomialBasis()
+θ = basis(munge(data.u),b_type)
+LinearAlgebra.cond(θ)
+ξ = _optimize(θ,v,STLSQ(0.01))
+pprint(ξ,b_type)
+
+_remake(ξ,b_type)
+savefig("../figures/Remade_Lotka.svg")
